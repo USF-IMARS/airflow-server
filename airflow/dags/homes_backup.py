a5f69a11708680d453643766bee5af74b665fd68
Tylar <email@tylar.info>
2024-05-28 11:33:49 -0400

    Create homes_backup.py

---
+++ airflow/dags/homes_backup.py
@@ -0,0 +1,25 @@
+"""
+backup of homes directory from yin to tpa pgs
+"""
+import os
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+with DAG(
+    'ts_ingest',
+    catchup=False,  # latest only
+    schedule_interval="0 6 * * 6",  # 6am every Sat
+    max_active_runs=1,
+    default_args={
+        "start_date": datetime(2024, 1, 1)
+    },
+) as dag:
+    BashOperator(
+        task_id="homes_backup_to_pgs,
+        bash_command=(
+            "rsync -habuP /srv/imars-objects/homes/ /srv/imars-objects/tpa_pgs/yin/homes/"
+        )
+    )
+    # TODO: add another BashOperator for secondary backup
